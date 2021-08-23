e962c64e091500c6798af2e4661bc510d219c695
ade pranaya 153040050 <ade.pranaya@mail.unpas.ac.id>
2021-08-23 23:10:03 +0700

    belajar tambah ubah hapus

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 
+const { body, validationResult, check } = require("express-validator");
+const methodOverride = require("method-override");
+
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const flash = require("connect-flash");
@@ -10,6 +13,9 @@ const Contact = require("./models/contact");
 
 const app = express();
 const port = 3000;
+
+// Setup Method Ovveride
+app.use(methodOverride("_method"));
 
 // Setup view engine (ejs)
 app.set("view engine", "ejs");
@@ -57,6 +63,118 @@ app.get("/contact", async (req, res) => {
     msg: req.flash("msg")
   });
 });
+
+// Halaman Form Tambah Data Contact
+app.get("/contact/add", (req, res) => {
+  res.render("add-contact", {
+    title: "Form Tambah Data Contact"
+  });
+});
+
+// Process Tambah Data Contact
+app.post(
+  "/contact",
+  [
+    body("nama").custom(async value => {
+      const duplikat = await Contact.findOne({ nama: value });
+      if (duplikat) {
+        throw new Error("Nama Contact sudah digunakan!");
+      }
+      return true;
+    }),
+    check("email").isEmail().withMessage("Email tidak valid!"),
+    check("nohp").isMobilePhone("id-ID").withMessage("No HP tidak valid!")
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // return res.status(400).json({ errors: errors.array() });
+      res.render("add-contact", {
+        title: "Form Tambah Data Contact",
+        errors: errors.array()
+      });
+    } else {
+      Contact.insertMany(req.body, (error, result) => {
+        // kirimkan flash message
+        req.flash("msg", "Data contact berhasil ditambahkan");
+        res.redirect("/contact");
+      });
+    }
+  }
+);
+// proses delete contact
+// app.get("/contact/delete/:nama", async (req, res) => {
+//   const contact = await Contact.findOne({ nama: req.params.nama });
+//   // jika kontak tidak ada
+//   if (!contact) {
+//     res.status(404);
+//     res.send("<h1>404</h1>");
+//   } else {
+//     Contact.deleteOne({ _id: contact._id }).then(() => {
+//       req.flash("msg", "Data contact berhasil dihapus");
+//       res.redirect("/contact");
+//     });
+//   }
+// });
+
+app.delete("/contact", (req, res) => {
+  Contact.deleteOne({ nama: req.body.nama }).then(result => {
+    req.flash("msg", "Data contact berhasil dihapus");
+    res.redirect("/contact");
+  });
+});
+
+// Halaman Form Ubah Data Contact
+app.get("/contact/edit/:nama", async (req, res) => {
+  const contact = await Contact.findOne({ nama: req.params.nama });
+  res.render("edit-contact", {
+    title: "Form Ubah Data Contact",
+    contact
+  });
+});
+
+// Process ubah Data Contact
+app.put(
+  "/contact",
+  [
+    body("nama").custom(async (value, { req }) => {
+      const duplikat = await Contact.findOne({ nama: value });
+      if (value !== req.body.oldNama && duplikat) {
+        throw new Error("Nama Contact sudah digunakan!");
+      }
+      return true;
+    }),
+    check("email").isEmail().withMessage("Email tidak valid!"),
+    check("nohp").isMobilePhone("id-ID").withMessage("No HP tidak valid!")
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render("edit-contact", {
+        title: "Form Ubah Data Contact",
+        errors: errors.array(),
+        contact: req.body
+      });
+    } else {
+      Contact.updateOne(
+        { _id: req.body._id },
+        {
+          $set: {
+            nama: req.body.nama,
+            email: req.body.email,
+            nohp: req.body.nohp
+          }
+        }
+      ).then(() => {
+        // kirimkan flash message
+        req.flash("msg", "Data contact berhasil diubah!");
+        res.redirect("/contact");
+      });
+    }
+  }
+);
 
 // Halaman Detail Contact
 app.get("/contact/:nama", async (req, res) => {
